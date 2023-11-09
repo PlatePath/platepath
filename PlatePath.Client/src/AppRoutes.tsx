9f036115b07b0c02b31443cd61b152756d73064e
isnomatophobic@gmail.com <isnomatophobic@gmail.com>
2023-11-09 12:51:29 +0200

    Test

--- PlatePath.Client/src/AppRoutes.tsx
+++ PlatePath.Client/src/AppRoutes.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<>Hello World</>} />
+      <Route path="/" element={<>Test pr</>} />
     </Routes>
   );
 };
