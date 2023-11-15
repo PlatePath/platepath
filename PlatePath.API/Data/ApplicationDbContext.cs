3a7013aaaffdb7bb511851ee746fc11c2b6a26ce
Веселин Инзов <veselin.inzov@gmail.com>
2023-11-15 21:20:30 +0200

    Added Identity, Roles, JWT Bearer, Login/Register logic

--- PlatePath.API/Data/ApplicationDbContext.cs
+++ PlatePath.API/Data/ApplicationDbContext.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace PlatePath.API.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
-        public ApplicationDbContext(DbContextOptions options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedRoles(builder);
+        }
+
         public DbSet<Recipe> Recipes { get; set; }
+
+        private static void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData
+                (
+                new IdentityRole { Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },
+                new IdentityRole { Name = "User", ConcurrencyStamp = "2", NormalizedName = "User" }
+                );
+        }
     }
 }
---
+++ PlatePath.API/Data/Models/Authentication/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace PlatePath.API.Data.Models.Authentication
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
