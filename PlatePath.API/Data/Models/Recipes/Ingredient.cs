e3882e81c8255ef46a814eff2c688ce1f3a22c21
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-20 13:39:44 +0200

    Map many-to-many relationship between Recipes and Ingredients.

--- PlatePath.API/Data/Models/Recipes/Ingredient.cs
+++ PlatePath.API/Data/Models/Recipes/Ingredient.cs
@@ -1,16 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlatePath.API.Data.Models.Recipes
 {
     public class Ingredient
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
         public string Name { get; set; }
-        
         public double Quantity { get; set; }
-        
         public string Measure { get; set; }
+
+        public List<Recipe> Recipes { get; set; } = new();
     }
 }
