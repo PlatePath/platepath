5bdce7e591de8091207c31ff008a21c2f93c0f8c
mertolbekirov <62621906+mertolbekirov@users.noreply.github.com>
2023-11-21 20:38:39 +0200

    51 create user entity and link needed proeprties (#52)
    * add user entity and add needed properties
    
    * fix enums / fix migrations
    
    * comments addressed- removed unneeded parameter in seeding methods
    
    - change user column

---
+++ PlatePath.API/Migrations/20231120231808_UserEntityCreated.cs
@@ -0,0 +1,359 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlatePath.API.Migrations
+{
+    public partial class UserEntityCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "16dfc99b-be89-4e30-9e1f-9feac6004bcc");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4f9b2648-1d91-474a-9056-e94fec669481");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Recipes",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "MealPlans",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ActivityLevelId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GenderId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "HeightCm",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "NeededCalories",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "NeededCarbs",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "NeededFats",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "NeededProtein",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "WeightCm",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WeightGoalId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ActivityLevel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityLevel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeightGoal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeightGoal", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ActivityLevel",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Sedentary" },
+                    { 2, "LightlyActive" },
+                    { 3, "ModeratelyActive" },
+                    { 4, "VeryActive" },
+                    { 5, "ExtraActive" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "046b0fb4-792a-4e6a-be57-055f6fb66002", "2", "User", "User" },
+                    { "f2e036f5-d0eb-46ef-a95d-79d563b41807", "1", "Admin", "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Male" },
+                    { 2, "Female" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "WeightGoal",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "LooseWeight" },
+                    { 2, "MaintainWeight" },
+                    { 3, "GainWeight" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_UserId",
+                table: "Recipes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MealPlans_UserId",
+                table: "MealPlans",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ActivityLevelId",
+                table: "AspNetUsers",
+                column: "ActivityLevelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_GenderId",
+                table: "AspNetUsers",
+                column: "GenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_WeightGoalId",
+                table: "AspNetUsers",
+                column: "WeightGoalId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_ActivityLevel_ActivityLevelId",
+                table: "AspNetUsers",
+                column: "ActivityLevelId",
+                principalTable: "ActivityLevel",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Genders_GenderId",
+                table: "AspNetUsers",
+                column: "GenderId",
+                principalTable: "Genders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_WeightGoal_WeightGoalId",
+                table: "AspNetUsers",
+                column: "WeightGoalId",
+                principalTable: "WeightGoal",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MealPlans_AspNetUsers_UserId",
+                table: "MealPlans",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_AspNetUsers_UserId",
+                table: "Recipes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_ActivityLevel_ActivityLevelId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Genders_GenderId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_WeightGoal_WeightGoalId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MealPlans_AspNetUsers_UserId",
+                table: "MealPlans");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_AspNetUsers_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "ActivityLevel");
+
+            migrationBuilder.DropTable(
+                name: "Genders");
+
+            migrationBuilder.DropTable(
+                name: "WeightGoal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MealPlans_UserId",
+                table: "MealPlans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ActivityLevelId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_GenderId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_WeightGoalId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "046b0fb4-792a-4e6a-be57-055f6fb66002");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f2e036f5-d0eb-46ef-a95d-79d563b41807");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "MealPlans");
+
+            migrationBuilder.DropColumn(
+                name: "ActivityLevelId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GenderId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "HeightCm",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NeededCalories",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NeededCarbs",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NeededFats",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NeededProtein",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WeightCm",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WeightGoalId",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "16dfc99b-be89-4e30-9e1f-9feac6004bcc", "1", "Admin", "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "4f9b2648-1d91-474a-9056-e94fec669481", "2", "User", "User" });
+        }
+    }
+}
