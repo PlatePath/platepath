45ad2c06a9d9bb3dd428d95ed26a1155b8e35644
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-21 21:09:23 +0200

    Add migration for the new Recipe fields.

--- PlatePath.API/Migrations/20231121190824_AddEdamamIdToRecipes.Designer.cs
+++ PlatePath.API/Migrations/20231121190824_AddEdamamIdToRecipes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlatePath.API.Data;
 
@@ -11,9 +12,10 @@ using PlatePath.API.Data;
 namespace PlatePath.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231121190824_AddEdamamIdToRecipes")]
+    partial class AddEdamamIdToRecipes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PlatePath.API/Migrations/20231121190824_AddEdamamIdToRecipes.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlatePath.API.Migrations
+{
+    public partial class AddEdamamIdToRecipes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "79046ee0-144f-49fb-94c6-1070d0e1d8c8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f1e928d7-775e-40b1-bf7e-86939afa81f9");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EdamamId",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "305ab135-e39b-4e8a-a05d-a3f60cf5c501", "1", "Admin", "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "8155b3fc-550d-453c-9a2d-ff83209d805c", "2", "User", "User" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "305ab135-e39b-4e8a-a05d-a3f60cf5c501");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8155b3fc-550d-453c-9a2d-ff83209d805c");
+
+            migrationBuilder.DropColumn(
+                name: "EdamamId",
+                table: "Recipes");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "79046ee0-144f-49fb-94c6-1070d0e1d8c8", "1", "Admin", "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f1e928d7-775e-40b1-bf7e-86939afa81f9", "2", "User", "User" });
+        }
+    }
+}
