781460064a872fc60e7da298bf29f4d98a644ccf
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-21 20:56:30 +0200

    Add MealPlanRequest class.
    The class maps a request to Edamam's meal plan API.

---
+++ PlatePath.API/Data/Models/MealPlans/MealPlanRequest.cs
@@ -0,0 +1,48 @@
+namespace PlatePath.API.Data.Models.MealPlans;
+
+public class MealPlanRequest
+{
+    public int size { get; set; }
+    public Plan plan { get; set; }
+}
+
+public class Plan
+{
+    public Accept accept { get; set; }
+    public Fit fit { get; set; }
+    public ResponseSections ResponseSections { get; set; }
+}
+
+public class Accept
+{
+    public List<All> all { get; set; }
+}
+
+public class All
+{
+    public List<string> health { get; set; }
+}
+
+public class Fit
+{
+    public ENERCKCAL ENERC_KCAL { get; set; }
+}
+
+public class ENERCKCAL
+{
+    public int min { get; set; }
+    public int max { get; set; }
+}
+
+public class RequestSections
+{
+    public RequestMeal Meal1 { get; set; }
+    public RequestMeal Meal2 { get; set; }
+    public RequestMeal Meal3 { get; set; }
+    public RequestMeal Meal4 { get; set; }
+    public RequestMeal Meal5 { get; set; }
+}
+
+public class RequestMeal
+{
+}
