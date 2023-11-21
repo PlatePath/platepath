75ae05e8fe6ce2e37072d5217fa36409d333ea05
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-21 21:27:54 +0200

    Create IAdminService interface and add methods to it.

--- PlatePath.API/Services/IAdminService.cs
+++ PlatePath.API/Services/IAdminService.cs
@@ -1,6 +1,10 @@
-﻿namespace PlatePath.API.Services
+﻿namespace PlatePath.API.Services;
+
+public interface IAdminService
 {
-    public interface IAdminService
-    {
-    }
+    void BanUser(int userId);
+
+    void DeleteForumPost(int postId);
+
+    void DeletePostComment(int postId, int commentId);
 }
