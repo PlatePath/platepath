71a32438275f8d0200a06035219b95d19b7af406
Kaloyan Parvanov <kaloyanparvanov00@gmail.com>
2023-11-22 13:24:28 +0200

    Create IForumService.
    This interface contains methods for the forum functionality.

--- PlatePath.API/Services/IForumService.cs
+++ PlatePath.API/Services/IForumService.cs
@@ -1,5 +1,26 @@
-﻿namespace PlatePath.API.Services;
+using PlatePath.API.Data.Models.Forum;
+
+namespace PlatePath.API.Services;
 
 public interface IForumService
 {
+    List<Post> GetPosts(int page, int itemsPerPage);
+    
+    List<Comment> GetPostComments(int postId);
+
+    List<Like> GetPostLikes(int postId);
+
+    List<Like> GetCommentLikes(int commentId);
+
+    Like LikePost(int userId, int postId);
+
+    Like LikeComment(int userId, int commentId);
+
+    Post CreatePost(Post post);
+
+    Post EditPost(int postId, Post post);
+
+    Comment CreateComment(Comment comment);
+
+    Comment EditComment(int commentId, Comment comment);
 }
