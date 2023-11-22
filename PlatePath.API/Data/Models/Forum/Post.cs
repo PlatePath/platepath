cb30a8612527c2cc6c531481c0172570036635d5
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-22 11:44:07 +0200

    Implement Post entity with a one-to-many relationship with the Comments.

---
+++ PlatePath.API/Data/Models/Forum/Post.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlatePath.API.Data.Models.Forum;
+
+public class Post
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public List<Comment> Comments { get; set; } = new();
+
+    /*public Recipe Recipe { get; set; }*/ // connect this to a recipe
+
+    // link this to the UserId that made the post
+}
