86a7bcb9e3b7a754d56db89d6c1b067381ee5ba1
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-22 11:31:19 +0200

    Implement some of the logic for banning users.

--- PlatePath.API/Services/AdminService.cs
+++ PlatePath.API/Services/AdminService.cs
@@ -1,6 +1,34 @@
-﻿namespace PlatePath.API.Services
+﻿using PlatePath.API.Data;
+using PlatePath.API.Data.Models.Users;
+
+namespace PlatePath.API.Services;
+
+public class AdminService : IAdminService
 {
-    public class AdminService : IAdminService
+    private readonly ApplicationDbContext dbContext;
+
+    public AdminService(ApplicationDbContext dbContext)
     {
+        this.dbContext = dbContext;
+    }
+    
+    public void BanUser(int userId)
+    {
+        User? user = dbContext.Users.Find(userId);
+        if (user == null)
+        {
+            // logic for user not found
+        }
+        // ban the user and maybe return it?
+    }
+
+    public void DeleteForumPost(int postId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeletePostComment(int postId, int commentId)
+    {
+        throw new NotImplementedException();
     }
 }
