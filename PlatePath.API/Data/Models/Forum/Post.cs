dcc20f1dc7c115a3c3d8b1ad141ccbfd613f5d4a
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-22 12:18:42 +0200

    Add UserId to the Posts.

--- PlatePath.API/Data/Models/Forum/Post.cs
+++ PlatePath.API/Data/Models/Forum/Post.cs
@@ -9,6 +9,8 @@ public class Post
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     
+    public int UserId { get; set; }
+    
     public string? Title { get; set; }
 
     public string? Description { get; set; }
