4c3aa2dc8f7e5859239458dc53c0be7f3c99b30c
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-23 12:35:43 +0200

    Add identity key generation for the Id property of Likes.

--- PlatePath.API/Data/Models/Forum/Like.cs
+++ PlatePath.API/Data/Models/Forum/Like.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace PlatePath.API.Data.Models.Forum;
 
 public class Like
 {
-    public int LikeId { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
     
     public int UserId { get; set; }
     
