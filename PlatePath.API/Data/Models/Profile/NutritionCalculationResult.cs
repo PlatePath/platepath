9936fda3edaad32d6749da656421127d74e0732c
mertolbekirov <mertol2002@gmail.com>
2023-11-28 01:38:22 +0200

    Create ProfileService, where we calculate the BMR

---
+++ PlatePath.API/Data/Models/Profile/NutritionCalculationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace PlatePath.API.Data.Models.Profile
+{
+    public class NutritionCalculationResult
+    {
+        public double Calories { get; set; }
+        public double ProteinGrams { get; set; }
+        public double FatGrams { get; set; }
+        public double CarbGrams { get; set; }
+    }
+}
--- PlatePath.API/Services/IProfileService.cs
+++ PlatePath.API/Services/IProfileService.cs
@@ -1,6 +1,9 @@
-﻿namespace PlatePath.API.Services
+﻿using PlatePath.API.Data.Models.Profile;
+
+namespace PlatePath.API.Services
 {
     public interface IProfileService
     {
+        NutritionCalculationResult CalculateNutrition(string userId);
     }
 }
