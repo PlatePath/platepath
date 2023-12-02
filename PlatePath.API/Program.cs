f16dfe836be90d067656ccb396b6d75faaa9d257
Веселин Инзов <veselin.inzov@gmail.com>
2023-12-02 22:00:08 +0200

    changed swagger doc title

--- PlatePath.API/Program.cs
+++ PlatePath.API/Program.cs
@@ -61,7 +61,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => {
     c.SwaggerDoc("v1", new OpenApiInfo
     {
-        Title = "JWTToken_Auth_API",
+        Title = "PlatePath.API",
         Version = "v1"
     });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
