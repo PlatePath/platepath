cec4bb138f42c1b9f18e4a5a6b11da3c89c0da96
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-12-05 20:45:38 +0200

    Add empty implementations for ForumService.

--- PlatePath.API/Services/ForumService.cs
+++ PlatePath.API/Services/ForumService.cs
@@ -1,5 +1,56 @@
-﻿namespace PlatePath.API.Services;
+﻿using PlatePath.API.Data.Models.Forum;
+
+namespace PlatePath.API.Services;
 
 public class ForumService : IForumService
 {
+    public List<Post> GetPosts(int page, int itemsPerPage)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Comment> GetPostComments(int postId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Like> GetPostLikes(int postId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Like> GetCommentLikes(int commentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Like LikePost(int userId, int postId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Like LikeComment(int userId, int commentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Post CreatePost(Post post)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Post EditPost(int postId, Post post)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Comment CreateComment(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Comment EditComment(int commentId, Comment comment)
+    {
+        throw new NotImplementedException();
+    }
 }
