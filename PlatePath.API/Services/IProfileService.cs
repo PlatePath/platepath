b143c50caf43dfdaa8d297351fd8c10b7cf8235d
mertolbekirov <62621906+mertolbekirov@users.noreply.github.com>
2023-12-06 22:43:10 +0200

    create profile controller and add needed service methods (#67)

--- PlatePath.API/Services/IProfileService.cs
+++ PlatePath.API/Services/IProfileService.cs
@@ -1,8 +1,11 @@
 ﻿using PlatePath.API.Data.Models.Profile;
+using PlatePath.API.Models.InputModels.User;
 
 namespace PlatePath.API.Services;
 
 public interface IProfileService
 {
     NutritionCalculationResult CalculateNutrition(string userId);
+
+    bool SetUserPersonalData(UserPersonalDataInputModel request, string userId);
 }
