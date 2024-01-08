392fd91aa1bbad3bab3691a6b2a8b185997c92c0
mertolbekirov <mertol2002@gmail.com>
2024-01-08 19:32:04 +0200

    fix getneedednutrition

--- PlatePath.API/Services/ProfileService.cs
+++ PlatePath.API/Services/ProfileService.cs
@@ -60,7 +60,7 @@ public class ProfileService : IProfileService
 
     public NutritionCalculationResult CalculateNutrition(string userId)
     {
-        var user = _context.Users.FirstOrDefault(u => u.Id == userId);
+        var user = _context.Users.Where(u => u.Id == userId).Include(x => x.Gender).Include(x => x.ActivityLevel).Include(x => x.WeightGoal).FirstOrDefault();
         if (user == null)
             throw new ArgumentException("User not found.", nameof(userId));
 
